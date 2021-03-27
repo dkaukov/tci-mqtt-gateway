8de953664a402690bac9adc6042f1fd2dfbacd9b
Dmitry Kaukov (OSS) <dkaukov@opentable.com>
2021-03-28 02:07:21 +1100

    WIP

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const { Factory } = require('winston-simple-wrapper')
+const log = new Factory({
+  transports: [{
+      type: 'console',
+      level: 'debug'
+    }, {
+      type: 'file',
+      level: 'debug',
+      filename: 'logs/gateway.log'
+  }]
+})
 const WSCLINET = require('ws-reconnect');
 const config = require('config');
 const wsClient = new WSCLINET(config.get("SDR").tci, {
@@ -12,16 +23,16 @@ const mqttClient =  mqtt.connect(config.get("MQTT").uri, {reconnectPeriod: 5000}
 var trxState = {};
 
 mqttClient.on('connect', () => {
-    console.log('MQTT: Connected to: ' + config.get("MQTT").uri);
+    log.info('Connected to: ' + config.get("MQTT").uri, "MQTT");
     mqttClient.subscribe('tci-mqtt-gateway/raw/to-sdr');
 })
 
-mqttClient.on('error', function(error) {
-    console.log('MQTT: ' + error.toString());
+mqttClient.on('error', (error) => { 
+    log.info(error.toString(), "MQTT");
 });
 
-mqttClient.on("reconnect", function(){
-    console.log("MQTT: Reconnecting...");
+mqttClient.on("reconnect", () =>  {
+    log.warn("Reconnecting...", "MQTT");
 });
 
 mqttClient.on('message', (topic, message) => {
@@ -32,32 +43,31 @@ mqttClient.on('message', (topic, message) => {
     }
 })
 
-wsClient.on("reconnect", function(){
-    console.log("WS: Reconnecting...");
+wsClient.on("reconnect", () => {
+    log.warn("Reconnecting...", "WS");
 });
 
-wsClient.on('connect', function(connection) {
-    console.log('WS: Connected to: ' + config.get("SDR").tci);
+wsClient.on('connect', () => {
+    log.info('Connected to: ' + config.get("SDR").tci, "WS");
 });
 
-wsClient.on('message', function(message) {
-    //console.log("Received: '" + message + "'");
-    if (mqttClient.connected) {
-        mqttClient.publish("tci-mqtt-gateway/raw/from-sdr", message);
-        try {
-            const event = parser.parse(message);
-            console.log("TCI: " + JSON.stringify(event));
-            mqttClient.publish("tci-mqtt-gateway/events/from-sdr",  JSON.stringify(event));
-            Object.assign(trxState, event.data);
-            mqttClient.publish("tci-mqtt-gateway/state/trx",  JSON.stringify(trxState));
-        } catch (err) {
-            if (!err.hasOwnProperty('location')) throw(err);
-            //console.log('TCI parser error: ' + err);
-        }
+wsClient.on('message', (message) => {
+    //log.info("Received: '" + message + "'");
+    mqttClient.publish("tci-mqtt-gateway/raw/from-sdr", message);
+    try {
+        const event = parser.parse(message);
+        log.info("TCI: " + JSON.stringify(event), "WS");
+        mqttClient.publish("tci-mqtt-gateway/events/from-sdr",  JSON.stringify(event));
+        Object.assign(trxState, event.data);
+        mqttClient.publish("tci-mqtt-gateway/state/trx",  JSON.stringify(trxState));
+    } catch (err) {
+        if (!err.hasOwnProperty('location')) throw(err);
+        //log.info('TCI parser error: ' + err);
     }
 });
 
 async function start() {
+    log.info("Starting up.");
     wsClient.start();
 }
 
