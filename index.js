b1d988a9e333a680ab66cbf46ef580f8a79e16e2
Dmitry Kaukov (OSS) <dkaukov@opentable.com>
2021-05-24 12:09:56 +1000

    WIP

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const { Factory } = require('winston-simple-wrapper')
 const log = new Factory({
-  transports: [{
-      type: 'console',
-      level: 'silly'
+    transports: [{
+        type: 'console',
+        level: 'silly'
     }, {
-      type: 'file',
-      level: 'debug',
-      filename: 'logs/gateway.log'
-  }]
+        type: 'file',
+        level: 'debug',
+        filename: 'logs/gateway.log'
+    }]
 })
 const WSCLINET = require('ws-reconnect');
+process.env["NODE_CONFIG_DIR"] = __dirname + "/config/" + require('path').delimiter + "./config";
 const config = require('config');
 const wsClient = new WSCLINET(config.get("SDR").tci, {
-    retryCount: 1000, 
-    reconnectInterval: 5 
+    retryCount: 1000,
+    reconnectInterval: 5
 });
 const peg = require("pegjs");
 const fs = require('fs');
-const deserializer = peg.generate(fs.readFileSync('protocol/tci-deserializer.pegjs').toString());
+const deserializer = peg.generate(fs.readFileSync(__dirname + '/protocol/tci-deserializer.pegjs').toString());
 const mqtt = require('mqtt')
-const mqttClient =  mqtt.connect(config.get("MQTT").uri, {reconnectPeriod: 5000});
-const serializer = require("./protocol/tci-serializer");
-var trxState = {ready: false};
+const mqttClient = mqtt.connect(config.get("MQTT").uri, { reconnectPeriod: 5000 });
+const serializer = require(__dirname + "/protocol/tci-serializer");
+var trxState = { ready: false };
 
 mqttClient.on('connect', () => {
     log.info('Connected to: ' + config.get("MQTT").uri, "MQTT");
@@ -29,21 +30,21 @@ mqttClient.on('connect', () => {
     mqttClient.subscribe('tci-mqtt-gatewayv2/events/to-sdr');
 })
 
-mqttClient.on('error', (error) => { 
+mqttClient.on('error', (error) => {
     log.info(error.toString(), "MQTT");
 });
 
-mqttClient.on("reconnect", () =>  {
+mqttClient.on("reconnect", () => {
     log.warn("Reconnecting...", "MQTT");
 });
 
 mqttClient.on('message', (topic, message) => {
     if (wsClient.isConnected) {
-        if(topic === 'tci-mqtt-gateway/raw/to-sdr') {
+        if (topic === 'tci-mqtt-gateway/raw/to-sdr') {
             log.info(message.toString(), "RAW")
             wsClient.send(message.toString());
         }
-        if(topic === 'tci-mqtt-gatewayv2/events/to-sdr') {            
+        if (topic === 'tci-mqtt-gatewayv2/events/to-sdr') {
             try {
                 let msg = serializer.serialize(JSON.parse(message.toString()));
                 log.info(msg, "SER");
@@ -63,7 +64,7 @@ wsClient.on("reconnect", () => {
 
 wsClient.on('connect', () => {
     log.info('Connected to: ' + config.get("SDR").tci, "WS");
-    trxState = {ready: false};
+    trxState = { ready: false };
 });
 
 wsClient.on('message', (message) => {
@@ -72,15 +73,15 @@ wsClient.on('message', (message) => {
     try {
         const event = deserializer.parse(message);
         log.info("tci-mqtt-gateway/v2/events/from-sdr/" + event.topic() + " " + JSON.stringify(event), "WS");
-        mqttClient.publish("tci-mqtt-gateway/events/from-sdr",  JSON.stringify(event));
-        mqttClient.publish("tci-mqtt-gateway/v2/events/from-sdr/" + event.topic(),  JSON.stringify(event), {retain: true});
-        trxState =  event.toState(trxState);
+        mqttClient.publish("tci-mqtt-gateway/events/from-sdr", JSON.stringify(event));
+        mqttClient.publish("tci-mqtt-gateway/v2/events/from-sdr/" + event.topic(), JSON.stringify(event), { retain: true });
+        trxState = event.toState(trxState);
         if (trxState.ready) {
-            mqttClient.publish("tci-mqtt-gateway/state/trx",  JSON.stringify(trxState), {retain: true});
+            mqttClient.publish("tci-mqtt-gateway/state/trx", JSON.stringify(trxState), { retain: true });
             log.info("tci-mqtt-gateway/state/trx  " + JSON.stringify(trxState), "STATE");
         }
     } catch (err) {
-        if (!err.hasOwnProperty('location')) throw(err);
+        if (!err.hasOwnProperty('location')) throw (err);
         //log.silly('TCI parser error: ' + err);
     }
 });
